7aa0118cfd1d0120bf0f2012d6d5c2e44dd85944
Calvintoes <cxt6655@rit.edu>
2023-08-08 11:29:57 -0700

    added AF pinata link

---
+++ src/constant/data.ts
@@ -0,0 +1,23 @@
+/**
+ * Alienfrens Pinata URL
+ * To get specific NFT, add token id to the end of the URL after the "/" (forward-slash)
+ *
+ * Shape of data looks like:
+ * {
+ *  name: string
+ *  description: string
+ *  image: string
+ *  attributes: { trait_type: string, value: string }[]
+ * }
+ *
+ * List of attributes for the OG collection:
+ * - Background
+ * - Body
+ * - Clothes
+ * - Eyes
+ * - Mouth
+ * - Hats
+ */
+
+export const AF_URL =
+  "https://gateway.pinata.cloud/ipfs/QmNN69NeVQJ3iCscZvxgrzdUdRuXD3E7gRZWesDcEjpPTt/";
